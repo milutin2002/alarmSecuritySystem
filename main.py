f67f025c5d9d409f9257532412011380a057bc9d
milutin2002 <milutinjovanovic2002@gmail.com>
2025-07-02 00:32:00 +0200

    Added red led diode

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def motionDetected(pin):
     warnFlag=True
     print("Motion detected")
     
+led=machine.Pin(13,machine.Pin.OUT)
 sensor=machine.Pin(17,machine.Pin.IN)
 sensor.irq(trigger=machine.Pin.IRQ_RISING,handler=motionDetected)
 alarm=machine.Pin(15,machine.Pin.OUT)
 while True:
     if warnFlag==True:
         alarm.toggle()
+        led.on()
         warnFlag=False
-        time.sleep(3)
+        time.sleep(1)
         alarm.toggle()
+        led.off()
