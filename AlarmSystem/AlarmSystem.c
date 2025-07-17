7fb76ce26f9792a652dc1dd20318da59e6e21e8e
milutin <milutinjovanovic2002@gmail.com>
2025-07-17 22:07:23 +0200

    Dodao sam slanje signala

--- AlarmSystem/AlarmSystem.c
+++ AlarmSystem/AlarmSystem.c
@@ -5,7 +5,7 @@
 #define PIR_PIN 17
 #define LED_PIN 13
 #define ALARM_PIN 15
-
+#define SIGNAL_PIN 1
 absolute_time_t lastTriger;
 
 void initGpio(){
@@ -15,16 +15,21 @@ void initGpio(){
     gpio_set_dir(LED_PIN,GPIO_OUT);
     gpio_init(ALARM_PIN);
     gpio_set_dir(ALARM_PIN,GPIO_OUT);
+    gpio_init(SIGNAL_PIN);
+    gpio_set_dir(SIGNAL_PIN,GPIO_OUT);
+    gpio_put(SIGNAL_PIN,0);
 }
 void detectMotion(){
     absolute_time_t now=get_absolute_time();
     if(gpio_get(PIR_PIN) && absolute_time_diff_us(lastTriger,now)>2*1000 *10){
         gpio_put(ALARM_PIN,1);
         gpio_put(LED_PIN,1);
+        gpio_put(SIGNAL_PIN,1);
         sleep_ms(50);
         lastTriger=now;
         gpio_put(ALARM_PIN,0);
         gpio_put(LED_PIN,0);
+        gpio_put(SIGNAL_PIN,0);
     }
 }
 int main()
