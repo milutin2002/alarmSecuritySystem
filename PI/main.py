eb628b0d0f294863d2f235c087f1c43f02102758
milutin2002 <milutinjovanovic2002@gmail.com>
2025-07-17 22:28:05 +0200

    Dodao sam mogucnost ispisivanja detetekcije preko raspberry pi

---
+++ PI/main.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.IN)
+
+def motion_detected(channel):
+	print("Motion detected by Pico")
+	time.sleep(5)
+
+GPIO.add_event_detect(26,GPIO.RISING,callback=motion_detected)
+
+try:
+	while True:
+		time.sleep(1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
