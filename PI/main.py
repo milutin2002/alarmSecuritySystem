b67a8ff6ab147c1711f8de5ce43d3ab78d86e679
milutin2002 <milutinjovanovic2002@gmail.com>
2025-09-01 22:42:02 +0200

    Added uart for PI

--- PI/main.py
+++ PI/main.py
@@ -8,17 +8,19 @@ import cv2 as cv
 from datetime import datetime
 from pyfcm import FCMNotification
 from firebase_admin import credentials, messaging
-
+import serial
 
 load_dotenv()
 
 
 api_key_var=os.getenv("API_KEY")
 
+#serial config
+ser=serial.Serial("/dev/serial0",115200,timeout=1)
 
 #GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
 def capture_image():
 	cam=cv.VideoCapture(0)
@@ -56,14 +58,14 @@ def firebase_upload(image_path):
 def remove_file(image_path):
 	os.remove(image_path)
 
-def motion_detected(channel):
+def motion_detected():
 	print("Capturing image\n")
 	image_path=capture_image()
 	firebase_upload(image_path)
 	remove_file(image_path)
 	time.sleep(5)
 
-GPIO.add_event_detect(26,GPIO.RISING,callback=motion_detected)
+#GPIO.add_event_detect(26,GPIO.RISING,callback=motion_detected)
 
 
 #FIREBASE setup
@@ -81,6 +83,10 @@ firebase_admin.initialize_app(cred,{
 
 try:
 	while True:
+		data=ser.readline().decode(errors="ignore").strip()
+		if data and data=="Yes":
+			print(data)
+			motion_detected()
 		time.sleep(1)
 except KeyboardInterrupt:
 	GPIO.cleanup()
