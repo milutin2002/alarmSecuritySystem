5bd19677807fff44f5fa92a93c87a8f54fafa207
milutin2002 <milutinjovanovic2002@gmail.com>
2025-11-21 14:49:31 +0100

    Added stream

--- PI/main.py
+++ PI/main.py
@@ -10,8 +10,15 @@ from pyfcm import FCMNotification
 from firebase_admin import credentials, messaging
 import serial
 from gpiozero import Button
+from flask import Flask, Response
 
 load_dotenv()
+
+cam=cv.VideoCapture(0)
+
+
+app=Flask(__name__)
+
 
 
 api_key_var=os.getenv("API_KEY")
@@ -23,13 +30,28 @@ api_key_var=os.getenv("API_KEY")
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
+def generateFrame():
+	while True:
+		ret,frame=cam.read()
+		if not ret:
+			continue
+		ret,jpeg = cv.imencode(".jpg",frame)
+		bytes=jpeg.tobytes()
+		yield (b"--frame\r\n"+
+		b"Content-Type: image/jpeg\r\n\r\n"+
+		bytes+b"\r\n")
+
+@app.route('/stream')
+def stream():
+	return Response(generateFrame(),
+			mimetype='multipart/x-mixed-replace; boundary=frame'
+			)
+
 def capture_image():
-	cam=cv.VideoCapture(0)
 	ret,image=cam.read()
 	timestamp=datetime.now().strftime("%Y%m%d_%H%M%S")
 	filename=f"/home/milutin/alarmSecuritySystem/PI/{timestamp}.jpg"
 	cv.imwrite(filename,image)
-	cam.release()
 	return filename
 
 def firebase_upload(image_path):
@@ -81,12 +103,14 @@ firebase_admin.initialize_app(cred,{
 	'databaseURL':firebase_db
 })
 
-try:
-	GPIO.add_event_detect(26,GPIO.RISING,callback=motion_detected,bouncetime=200)
-	print("Added event")
-	#motion_sensor=Button(26,pull_up=False)
-	#motion_sensor.when_pressed=motion_detected
-	while True:
-		pass
-except KeyboardInterrupt:
-	GPIO.cleanup()
+if __name__ == "__main__":
+	try:
+		GPIO.add_event_detect(26,GPIO.RISING,callback=motion_detected,bouncetime=200)
+		print("Added event")
+		app.run(host="0.0.0.0",port=8080,threaded=True)
+		#motion_sensor=Button(26,pull_up=False)
+		#motion_sensor.when_pressed=motion_detected
+		while True:
+			pass
+	except KeyboardInterrupt:
+		GPIO.cleanup()
